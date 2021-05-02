8bb544373c9cd4d19c1a9b7fc313e6e27fb4fc65
ing <ingafter60@outlook.com>
2021-05-02 20:19:59 +0700

    8.8.24 Model - Create CustomUser model

--- app/adboard/models.py
+++ app/adboard/models.py
@@ -1,3 +1,9 @@
+# app/adboard/models.py
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+# CustomUser model
+class CustomUser(AbstractUser):
+	pass
